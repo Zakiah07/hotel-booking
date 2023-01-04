dd92f39af256f894a4580f907422e2ac6c29fda3
zakiah <zakiah_bloom@yahoo.com>
2023-01-04 21:41:25 +0800

    configuring server express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-const name = "RYAN";
-module.exports = {
-  name,
-};
+const express = require("express");
 
-console.log("====> Node Server", process.version, module.exports);
+const app = express();
+
+app.get("/api/:message", (req, res) => {
+  res.status(200).send(`Here is your message: ${req.params.message}`);
+});
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
