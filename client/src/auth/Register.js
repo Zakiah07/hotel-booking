0fa8ecb74564fea03430426a463e9add8bd129c5
zakiah <zakiah_bloom@yahoo.com>
2023-01-05 22:23:01 +0800

    updated env file

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -9,7 +9,41 @@ const Register = () => {
     alert("send user info to backend");
   };
 
-  const registerForm = () => <form onSubmit={handleSubmit}></form>;
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Your name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Email address</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
 
   return (
     <>
