d047cfeb5426a869d378541bef694cb12afc644d
zakiah <zakiah_bloom@yahoo.com>
2023-01-06 20:01:30 +0800

    user register

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import Login from "./auth/Login";
 import Register from "./auth/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopNav from "./components/TopNav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <Router>
       <TopNav />
+      <ToastContainer position="top-center" />
       <Routes>
         <Route
           path="/"
--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,58 +1,54 @@
 import React, { useState } from "react";
+import RegisterForm from "../components/RegisterForm";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = () => {
-    alert("send user info to backend");
-  };
+  const navigate = useNavigate();
 
-  const registerForm = () => (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Your name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label>Email address</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button className="btn btn-primary">Submit</button>
-    </form>
-  );
+  // console.log("history", history);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`http://localhost:8000/api/register`, {
+        name,
+        email,
+        password,
+      });
+      console.log("REGISTER USER ===>", res);
+      toast.success("Register success. Please login.");
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      if (err.response.status === 400) toast.error(err.response.data);
+    }
+  };
 
   return (
     <>
       <div className="container-fluid p-5 text-center bg-secondary ">
         <h1>Register </h1>
       </div>
+
       <div className="container">
         <div className="row">
-          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+          <div className="col-md-6 offset-md-3">
+            <RegisterForm
+              handleSubmit={handleSubmit}
+              name={name}
+              setName={setName}
+              email={email}
+              setEmail={setEmail}
+              password={password}
+              setPassword={setPassword}
+            />
+          </div>
         </div>
       </div>
     </>
