fe179d4d8bdb53301666d6843da36e4af7476e89
zakiah <zakiah_bloom@yahoo.com>
2023-01-30 16:58:25 +0800

    stripe settings for checkout

--- client/src/hotels/ViewHotel.js
+++ client/src/hotels/ViewHotel.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { read, diffDays } from "../actions/hotel";
+import moment from "moment";
+import { useSelector } from "react-redux";
+import { getSessionId } from "../actions/stripe";
 
 const ViewHotel = () => {
-  return <>single hotel</>;
+  const [hotel, setHotel] = useState({});
+  const [image, setImage] = useState("");
+  const { hotelId } = useParams();
+  const navigate = useNavigate();
+
+  const { auth } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadSellerHotel();
+  }, []);
+
+  const loadSellerHotel = async () => {
+    let res = await read(hotelId);
+    // console.log(res);
+    setHotel(res.data);
+    setImage(`${process.env.REACT_APP_API}/hotel/image/${res.data._id}`);
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (!auth) navigate("/login");
+    console.log(auth.token, hotelId);
+    let res = await getSessionId(auth.token, hotelId);
+    console.log("get session id", res.data.sessionId);
+  };
+
+  return (
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h2>{hotel.title}</h2>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <br />
+            <img
+              src={image}
+              alt={hotel.title}
+              className="img img-fluid m-2"
+            />
+          </div>
+          <div className="col-md-6">
+            <br />
+            <b>{hotel.content}</b>
+            <p className="alert alert-info mt-3">{hotel.price}</p>
+            <p className="card-text">
+              <span className="float-right text-primary">
+                for {diffDays(hotel.from, hotel.to)}{" "}
+                {diffDays(hotel.from, hotel.to) <= 1 ? "day" : "days"}
+              </span>
+            </p>
+            <p>
+              From <br />
+              {moment(new Date(hotel.from)).format("MMMM Do YYYY, h:mm:ss a")}
+            </p>
+            <p>
+              To <br />
+              {moment(new Date(hotel.to)).format("MMMM Do YYYY, h:mm:ss a")}
+            </p>
+            <i>Posted by {hotel.postedBy && hotel.postedBy.name}</i>
+            <br />
+            <button
+              onClick={handleClick}
+              className="btn btn-block btn-lg btn-primary mt-3 "
+            >
+              {auth && auth.token ? "Book Now" : "Login to Book"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ViewHotel;
--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -68,6 +68,7 @@ export const remove = async (req, res) => {
 
 export const read = async (req, res) => {
   let hotel = await Hotel.findById(req.params.hotelId)
+    .populate("postedBy", "_id name")
     .select("-image.data")
     .exec();
   console.log("SINGLE HOTELS");
